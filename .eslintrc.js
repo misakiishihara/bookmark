ca2c30df68cfe62edf8a8ad425b211d488bc0b81
{自分の名前} <misaki.ishihara0004@gmail.com>
2022-06-29 20:35:10 +0900

    set up

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,24 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'standard',
-    'prettier',
-  ],
+  extends: ['plugin:react/recommended', 'standard', 'prettier'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
   },
-  plugins: [
-    'react',
-    '@typescript-eslint'
-  ],
+  plugins: ['react', '@typescript-eslint'],
   rules: {
+    'react/react-in-jsx-scope': 'off',
   },
   settings: {
     react: {
-      version: '18.1.0'
-    }
-  }
-}
+      version: '18.1.0',
+    },
+  },
+};
